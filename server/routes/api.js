1bf8bc17bfbf73747526b010d8485f02afbced3f
zoukmer <merzouk1985@gmail.com>
2020-04-08 18:47:58 +0200

    add mongoose and mongodb connextion

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose')
+
+var options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+const db = 'mongodb+srv://merzouk:merzouk@cluster0-nu7un.mongodb.net/test?retryWrites=true&w=majority'
+
+
+mongoose.connect(db, options, (err) => {
+    if(err)
+        console.log(`Error ! ${err}`)
+    else
+        console.log('connnected to mongodb')
+})
+
 router.get('/', (req, res) => {
     res.send("From api router");
 });
