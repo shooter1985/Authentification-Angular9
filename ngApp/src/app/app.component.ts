4e74ec1e32462f41fbcf6bdfa68b14f1cb184b88
zoukmer <merzouk1985@gmail.com>
2020-04-10 19:55:25 +0200

    ajouter de logout

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngApp';
+export class AppComponent implements OnInit{
+  
+  constructor(private _authService: AuthService, private _router: Router){
+
+  }
+
+  ngOnInit(){
+  }
+  isloggedIN(){
+    return this._authService.loggedIn();
+  }
+
+  logout(){
+    this._authService.logoutUser()
+  }
+
+  
 }
--- ngApp/src/app/service/auth.service.ts
+++ ngApp/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthService {
 
   private _registerUrl = "http://localhost:3000/api/register"
   private _loginUrl = "http://localhost:3000/api/login"
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _router: Router) { }
 
   registerUser(user){
     return this.http.post<any>(this._registerUrl, user);
@@ -25,5 +26,10 @@ export class AuthService {
 
   getToken(){
     return localStorage.getItem('token')
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token')
+    this._router.navigate(['/events'])
   }
 }
