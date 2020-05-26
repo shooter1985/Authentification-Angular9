618809e11b1e25200e754a1b4281b85e13f7ae99
lechercheur <merzouk1985@gmail.com>
2020-05-26 10:29:13 +0200

    add async await for the login api nd make some modification in the front

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,41 +5,40 @@ const tokenVerify = require('../middlewarefunction/verifytoken')
 /*const paginate = require('jw-paginate')
 const mongoosePaginate = require('mongoose-paginate')*/
 
-let saltRounds = parseInt(process.env.SaltRounds) 
+let saltRounds = parseInt(process.env.SaltRounds)
 
 const router = express.Router();
 
 const User = require('../models/user')
 
 router.get('/', (req, res) => {
-    res.send("From api router");
+    res.send("From api router"); 
 });
 
 router.post('/register', (req, res) => {
     let userData = req.body;
     // hashage du password
-    User.findOne({email:userData.email}, (err, result) => {
-        if(err) {
+    User.findOne({ email: userData.email }, (err, result) => {
+        if (err) {
             res.status(401).send("Erreur Serveur");
-        } else {
-            if(result)
-            {
+        } else {err
+            if (result) {
                 res.status(401).send('Email déja pris !!');
             } else {
                 bcrypt.genSalt(saltRounds, (err, salt) => {
-                    bcrypt.hash(userData.password, salt, function(err, hash) {
-                        if(err) {
+                    bcrypt.hash(userData.password, salt, function (err, hash) {
+                        if (err) {
                             res.status(401).send('Register Cancel, retry!!!');
-                        }else {
+                        } else {
                             userData.password = hash
                             let user = new User(userData)
                             user.save((err, registeredUser) => {
                                 if (err) {
                                     res.status(401).send('Erreur du serveur!!');
                                 } else {
-                                    let payload = {subject: registeredUser._id}
+                                    let payload = { subject: registeredUser._id }
                                     let token = jwt.sign(payload, process.env.SECRETKEY)
-                                    res.status(200).send({token,registeredUser:{email: registeredUser.email,_id: registeredUser._id, nom:registeredUser.nom}});
+                                    res.status(200).send({ token, registeredUser: { email: registeredUser.email, _id: registeredUser._id, nom: registeredUser.nom } });
                                 }
                             })
                         }
@@ -50,27 +49,26 @@ router.post('/register', (req, res) => {
     })
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     let userData = req.body;
-    User.findOne({email:userData.email}, (err, user) => {
-        if(err) {
-            console.log(err);
+    try {
+        const user = await User.findOne({ email: userData.email });
+        if (!user) {
+            res.status(401).send('Invalid Email')
         } else {
-            if(!user) {
-                res.status(401).send ('Invalid Email')
+            const result = await bcrypt.compare(userData.password, user.password);
+            if (result) {
+                let payload = { subject: user._id }
+                let token = jwt.sign(payload, process.env.SECRETKEY)
+                res.status(200).send({ token, user: { email: user.email, _id: user._id, nom: user.nom } });
             } else {
-                bcrypt.compare(userData.password, user.password, (err, result) => {
-                    if(result) {
-                        let payload = {subject: user._id}
-                        let token = jwt.sign(payload, process.env.SECRETKEY)
-                        res.status(200).send ({token,user:{email: user.email,_id: user._id, nom:user.nom}});
-                    } else {
-                        res.status(401).send ('Invalid Password');
-                    } 
-                });
+                res.status(401).send('Invalid Password');
             }
         }
-    })
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 module.exports = router
